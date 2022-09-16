12f9b8ba79c69457eedb7b221b235a13f959f60f
ashish200214 <64855416+ashish200214@users.noreply.github.com>
2022-09-16 15:45:55 +0530

    Create arithmatic.py

---
+++ arithmatic.py
@@ -0,0 +1,8 @@
+#Arithmatic Operation
+num1=int(input("enter the num1"))
+num2=int(input("enter the num2"))
+print("Addition of number is = ",num1+num2)
+print("Substraction of number is =",num1-num2)
+print("Multiplication of number is = ",num1*num2)
+print("Division of number is = ",num1/num2)
+print("Modulus of number is = ",num1%num2)
