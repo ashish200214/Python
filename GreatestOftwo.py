c4e7135d378e4af45ac00361346fe0e623e80ff3
Ashish Gawali <64855416+ashish200214@users.noreply.github.com>
2023-08-26 18:34:05 +0530

    Create GreatestOftwo.py

---
+++ GreatestOftwo.py
@@ -0,0 +1,11 @@
+def greatest(num1,num2):
+    if(num1>num2):
+        print("num1 is greatest")
+    else:
+        print("num2 is greatest")
+
+
+num1 = int(input("Enter num1"))
+num2 = int(input("Enter num2"))
+
+greatest(num1,num2)
